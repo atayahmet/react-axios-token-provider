aa679a36d162e5485d05ac60eadf5d388f9ad7fa
Ahmet Atay <ahmet.atay@akinon.com>
2022-12-01 21:39:02 +0300

    feat: added the new paths to the default access token path variants

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,11 @@ export const ACCESS_TOKEN_KEY = 'accessToken';
 export const REFRESH_TOKEN_KEY = 'refreshToken';
 export const CSRF_TOKEN_KEY = 'csrfToken';
 
-export const DEFAULT_ACCESS_TOKEN_PATHS = ['headers.x-access-token', 'data.access_token'];
+export const DEFAULT_ACCESS_TOKEN_PATHS = [
+  'headers.x-access-token',
+  'headers.x-auth-token',
+  'data.auth_token',
+  'data.access_token',
+];
 export const DEFAULT_CSRF_TOKEN_PATHS = ['headers.x-csrf-token', 'headers.x-xsrf-token'];
 export const DEFAULT_REFRESH_TOKEN = ['headers.x-refresh-token', 'data.refresh_token'];
